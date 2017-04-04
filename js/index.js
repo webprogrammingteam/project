f84ea1e361bdaee4861b26923af31601730796b6
amberhasan <amber.hasan@gmail.com>
2017-04-04 11:43:37 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,15 +1,11 @@
 $(document).ready(function(){
-  alert("ok");
 
          $.ajax({
             type: 'GET',
             url: 'php/index.php',
             // data: {username: uname, password: pwd, email: email, fname: fname, lname: lname, address: addr, phone: phone},
             success: function(msg) {
-
-              alert("success");
-              $("#listOfProducts").html(msg);
-
+              $("#listOfProducts").html(msg); //lists the products and all from php
             },
             error: function(){
 
